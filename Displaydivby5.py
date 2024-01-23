054e13c7bd779b1e3357c25eb5a8b1d0b29681c0
ValtheCoder <keshia.kdf@gmail.com>
2024-01-23 20:13:42 +0800

    Set a function for Divisible by 5 and print

--- Displaydivby5.py
+++ Displaydivby5.py
@@ -4,3 +4,6 @@ print("Given list is ", num_x)
 print("Divisible by 5")
 # Set a function for Divisible 5
 # Print in list the numbers that are divisible by 5
+for num in num_x:
+    if num % 5 == 0:
+        print(num)
