a2bdee58486a8e460acfc8c1d53bf9761e1e8a7d
ValtheCoder <keshia.kdf@gmail.com>
2024-01-23 20:11:53 +0800

    Type the given and print

--- Displaydivby5.py
+++ Displaydivby5.py
@@ -1,3 +1,6 @@
 # Type the given
+num_x = [10, 20, 33, 46, 55]
+print("Given list is ", num_x)
+print("Divisible by 5")
 # Set a function for Divisible 5
 # Print in list the numbers that are divisible by 5
